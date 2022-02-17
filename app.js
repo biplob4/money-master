f22bf54e05cae6fbcaca7444b0b9adb788889cab
biplob4 <biplobhossen6020@gmail.com>
2022-02-17 18:38:49 +0600

    add all javascript

--- app.js
+++ app.js
@@ -9,51 +9,52 @@ function allInputValue(inputId) {
 
 
 /* ----set calculate button event Handler---- */
-document.getElementById('calculate-btn').addEventListener('click', function(){
+document.getElementById('calculate-btn').addEventListener('click', function () {
     const incomeAmount = allInputValue("income");
     const foodAmount = allInputValue("food");
     const rantAmount = allInputValue("rant");
     const clothesAmount = allInputValue("clothes");
 
-    /* ----set balance---- */
-    const expensed = document.getElementById("expensed-balance");
-    let expensedText = parseFloat(expensed.innerText);
-    if (incomeAmount > 0 && foodAmount > 0 && rantAmount > 0 && clothesAmount > 0) {
-        expensedText = expensed.innerText = foodAmount + rantAmount + clothesAmount;
-    }else{
-        document.getElementById('latter-error').style.display = 'block';
-    }
-
-    const totalbalance = document.getElementById("total-balance");
-    const totalbalanceText = parseFloat(totalbalance.innerText);
-    if (incomeAmount > 0 && foodAmount > 0 && rantAmount > 0 && clothesAmount > 0) {
-        totalbalance.innerText = incomeAmount - expensedText;
-    }
-
     /* ---error mesege---- */
-    if (incomeAmount < 0){
-        document.getElementById('income-error').style.display = 'block';
+    if (incomeAmount > 0 && foodAmount > 0 && rantAmount > 0 && clothesAmount > 0) {
+        const expensed = document.getElementById("expensed-balance");
+        let expensedText = parseFloat(expensed.innerText);
+        expensedText = expensed.innerText = foodAmount + rantAmount + clothesAmount;
+
+        const totalbalance = document.getElementById("total-balance");
+        const totalbalanceText = parseFloat(totalbalance.innerText);
+        totalbalance.innerText = incomeAmount - expensedText;
+
+        if (incomeAmount < expensedText) {
+            document.getElementById('expenses-error').style.display = 'block';
+        }
     }
-    if (foodAmount < 0){
-        document.getElementById('food-error').style.display = 'block';
+
+
+    else if (incomeAmount <= -0 || foodAmount <= -0 || rantAmount <= -0 || clothesAmount <= -0) {
+        if (incomeAmount < 0) {
+            document.getElementById('income-error').style.display = 'block';
+        }
+        if (foodAmount < 0) {
+            document.getElementById('food-error').style.display = 'block';
+        }
+        if (rantAmount < 0) {
+            document.getElementById('rant-error').style.display = 'block';
+        }
+        if (clothesAmount < 0) {
+            document.getElementById('clothes-error').style.display = 'block';
+        }
     }
-    if (rantAmount < 0){
-        document.getElementById('rant-error').style.display = 'block';
-    }
-    if (clothesAmount < 0){
-        document.getElementById('clothes-error').style.display = 'block';
-    }
-    if(incomeAmount < expensedText){
-        document.getElementById('expenses-error').style.display = 'block';
+    else {
+        document.getElementById('latter-error').style.display = 'block';
     }
 })
 
 
-
 /* -----event Handler set sevbutton------ */
-document.getElementById('save-btn').addEventListener('click', function (){
+document.getElementById('save-btn').addEventListener('click', function () {
     const expensed = document.getElementById("expensed-balance");
-    let expensedText = parseFloat(expensed.innerText);
+    const expensedText = parseFloat(expensed.innerText);
 
     const totalbalance = document.getElementById("total-balance");
     const totalbalanceText = parseFloat(totalbalance.innerText);
@@ -67,5 +68,19 @@ document.getElementById('save-btn').addEventListener('click', function (){
     const incomeAmount = allInputValue("income");
     const saveAmount = allInputValue("save");
 
+    /* ---error mesege---- */
+    if (saveAmount > 0) {
+        savebalance.innerText = incomeAmount / 100 * saveAmount;
+        remainingbalance.innerText = totalbalanceText - incomeAmount / 100 * saveAmount
+    }
+    else if (saveAmount <= -0) {
+        document.getElementById('morecost-error').style.display = 'block';
+    }
+    else {
+        document.getElementById('latter-error').style.display = 'block';
+    }
 
+    if(totalbalanceText > expensedText){
+        document.getElementById('save-error').style.display = 'block';
+    }
 })
