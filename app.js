50aa9e7a64410e44340f2dc2255710b0118be638
biplob4 <biplobhossen6020@gmail.com>
2022-02-17 13:53:14 +0600

    add.javascript

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-/*----- cought inputId function----- */
+
+
+/*-----cought allInputId----- */
 function allInputValue(inputId) {
     const allInput = document.getElementById(inputId + "-allInput");
     const inputText = allInput.value;
-    const inputValu = parseFloat(inputText);
-    // allInput.value = '';
-    return inputValu;
+    return inputText;
 }
 
 
@@ -13,28 +13,32 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     /* -----allInputValue------ */
     const incomeAmount = allInputValue("income");
+    const incomeAmountParse = parseFloat(incomeAmount);
+
     const foodAmount = allInputValue("food");
+    const foodAmountParse = parseFloat(foodAmount);
+
     const rantAmount = allInputValue("rant");
+    const rantAmountParse = parseFloat(rantAmount);
+    
     const clothesAmount = allInputValue("clothes");
+    const clothesAmountParse = parseFloat(clothesAmount);
 
     /* ----set balance---- */
     const expensed = document.getElementById( "expensed-balance");
     const expensedText = parseFloat(expensed.innerText);
-    const totalExpemsed = expensed.innerText = foodAmount + rantAmount + clothesAmount;
+    const totalExpemsed = expensed.innerText = foodAmountParse + rantAmountParse + clothesAmountParse;
 
     const totalbalance = document.getElementById( "total-balance");
     const totalbalanceText = parseFloat(totalbalance.innerText);
-    totalbalance.innerText = incomeAmount - totalExpemsed;
-    
-    if(incomeAmount < expensed){
-        document.getElementById('food-error').style.display='block';
-    }
+    totalbalance.innerText = incomeAmountParse - totalExpemsed;
+
 
     /* ---error mesege---- */
     if( incomeAmount < 0 ){
         document.getElementById('income-error').style.display='block';
        }
-    if( foodAmount < 0){
+    if( foodAmount < 0 && typeof inputText == 'string'){
         document.getElementById('food-error').style.display='block';
        }
     if( rantAmount < 0){
@@ -43,8 +47,7 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     if( clothesAmount < 0){
         document.getElementById('clothes-error').style.display='block';
        }
+    if(incomeAmount < totalExpemsed){
+        document.getElementById('expenes-error').style.display='block';
+    }
 })
- 
-// document.getElementById("calculate-btn").addEventListener('click',function(){
-    
-// })
