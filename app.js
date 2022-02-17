6f23a931a503d07ad0db0ad84175cad5f545ce71
biplob4 <biplobhossen6020@gmail.com>
2022-02-17 16:20:50 +0600

    chack push

--- app.js
+++ app.js
@@ -1,53 +1,71 @@
 
-
 /*-----cought allInputId----- */
 function allInputValue(inputId) {
     const allInput = document.getElementById(inputId + "-allInput");
-    const inputText = allInput.value;
+    const inputText = parseFloat(allInput.value);
     return inputText;
 }
 
 
+
 /* ----set calculate button event Handler---- */
-document.getElementById('calculate-btn').addEventListener('click', function () {
-
-    /* -----allInputValue------ */
+document.getElementById('calculate-btn').addEventListener('click', function(){
     const incomeAmount = allInputValue("income");
-    const incomeAmountParse = parseFloat(incomeAmount);
-
     const foodAmount = allInputValue("food");
-    const foodAmountParse = parseFloat(foodAmount);
-
     const rantAmount = allInputValue("rant");
-    const rantAmountParse = parseFloat(rantAmount);
-    
     const clothesAmount = allInputValue("clothes");
-    const clothesAmountParse = parseFloat(clothesAmount);
 
     /* ----set balance---- */
-    const expensed = document.getElementById( "expensed-balance");
-    const expensedText = parseFloat(expensed.innerText);
-    const totalExpemsed = expensed.innerText = foodAmountParse + rantAmountParse + clothesAmountParse;
+    const expensed = document.getElementById("expensed-balance");
+    let expensedText = parseFloat(expensed.innerText);
+    if (incomeAmount > 0 && foodAmount > 0 && rantAmount > 0 && clothesAmount > 0) {
+        expensedText = expensed.innerText = foodAmount + rantAmount + clothesAmount;
+    }else{
+        document.getElementById('latter-error').style.display = 'block';
+    }
 
-    const totalbalance = document.getElementById( "total-balance");
+    const totalbalance = document.getElementById("total-balance");
     const totalbalanceText = parseFloat(totalbalance.innerText);
-    totalbalance.innerText = incomeAmountParse - totalExpemsed;
-
+    if (incomeAmount > 0 && foodAmount > 0 && rantAmount > 0 && clothesAmount > 0) {
+        totalbalance.innerText = incomeAmount - expensedText;
+    }
 
     /* ---error mesege---- */
-    if( incomeAmount < 0 ){
-        document.getElementById('income-error').style.display='block';
-       }
-    if( foodAmount < 0 && typeof inputText == 'string'){
-        document.getElementById('food-error').style.display='block';
-       }
-    if( rantAmount < 0){
-        document.getElementById('rant-error').style.display='block';
-       }
-    if( clothesAmount < 0){
-        document.getElementById('clothes-error').style.display='block';
-       }
-    if(incomeAmount < totalExpemsed){
-        document.getElementById('expenes-error').style.display='block';
+    if (incomeAmount < 0){
+        document.getElementById('income-error').style.display = 'block';
     }
+    if (foodAmount < 0){
+        document.getElementById('food-error').style.display = 'block';
+    }
+    if (rantAmount < 0){
+        document.getElementById('rant-error').style.display = 'block';
+    }
+    if (clothesAmount < 0){
+        document.getElementById('clothes-error').style.display = 'block';
+    }
+    if(incomeAmount < expensedText){
+        document.getElementById('expenses-error').style.display = 'block';
+    }
+})
+
+
+
+/* -----event Handler set sevbutton------ */
+document.getElementById('save-btn').addEventListener('click', function (){
+    const expensed = document.getElementById("expensed-balance");
+    let expensedText = parseFloat(expensed.innerText);
+
+    const totalbalance = document.getElementById("total-balance");
+    const totalbalanceText = parseFloat(totalbalance.innerText);
+
+    const savebalance = document.getElementById("saving-balance");
+    const savebalanceText = parseFloat(totalbalance.innerText);
+
+    const remainingbalance = document.getElementById("remaining-balance");
+    const remainingbalanceText = parseFloat(totalbalance.innerText);
+
+    const incomeAmount = allInputValue("income");
+    const saveAmount = allInputValue("save");
+
+
 })
